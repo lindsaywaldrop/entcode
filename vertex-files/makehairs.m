af333cad47485f568a7be0beebd461210bfd46dd
Lindsay Waldrop <lindsay.waldrop@nmt.edu>
2017-09-20 15:22:42 -0600

    Several files updated and added

---
+++ vertex-files/makehairs.m
@@ -0,0 +1,16 @@
+% makehairs.m
+
+data = load('HairArray_para_1233.dat');
+
+%handle the data
+
+Gap = data(:,2);
+angle = data(:,1);
+
+% Generate hair files
+
+for i = 1:size(data,1)
+   
+   [p] = generate_grid2d(Gap(i),0.02,angle(i),i);
+    
+end
