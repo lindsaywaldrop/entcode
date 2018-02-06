7f68e1ab350c0083e2bf6ca6caa43a89016bad0d
Lindsay Waldrop <lindsay.waldrop@nmt.edu>
2018-02-06 15:21:49 -0700

    Adding running file for Bridges

--- entsniff.m
+++ entsniff.m
@@ -1,4 +1,7 @@
-% convert_entcode.m
+% entsniff.m
+%
+% Script for running code on Bridges
+%
 %	This script imports SAMRAI data into Matlab, interpolates to a
 %	uniform grid. Only the final time step (set by user) is imported.
 %
@@ -10,6 +13,9 @@
 % 	n = total number of simulations
 %	pathbase = where runs are located
 
+% Add paths to relevant matlab analysis scripts
+addpath(genpath('/pylon5/ca4s8kp/lwaldrop/entcode/'))
+
 close all
 clear all
 clc
@@ -18,13 +24,14 @@ GridSize = 4096;        %Size of the finest grid
 final_time=25000;      %Final time step to be included in data analysis
 n=1233; %Number of simulations
 
-pathbase='/Users/Bosque/IBAMR/entcode/code/runs/';
+pathbase1='/pylon5/ca4s8kp/lwaldrop/entcode/runs/';
+pathbase2='/pylon5/ca4s8kp/lwaldrop/entcode/hairinfo_files/';
 
 for i=1:n
     %i=3
     clear V Vinterp x y
-    file=['viz_IB2d',num2str(i)];
-    path1=[pathbase,file];
+    file1=['viz_IB2d',num2str(i)];
+    path1=[pathbase1,file1];
     
     disp(['Simulation number: ',num2str(i)])
     
@@ -34,11 +41,14 @@ for i=1:n
     %This reads the samrai data and interpolates it.
     [x,y,Vinterp,V] = importsamrai(path_name1,'interpolaten',[GridSize GridSize]);
     
-    %Shilpa's code goes here. 
-    w = 
+    % Loads hairdata
+    load([pathbase2,'hairinfo',num2str(i),'.mat'])
+    
+    % Run Shilpa's code here.
+    j = i.^2;
+    disp(num2str(j))
     
     % Saves data relevant to next step in workflow.
-    % Note: to tighten workflow, we won't be saving this step. 
     % save([pathbase,'/matfiles/',file,'.mat'],'V','Vinterp','x','y','GridSize','final_time')
 
 end
