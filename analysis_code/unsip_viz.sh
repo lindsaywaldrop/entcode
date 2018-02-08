256be06dce4f140d66cf201f0234b76c12a1cecd
Lindsay Waldrop <lindsay.waldrop@nmt.edu>
2018-02-08 06:21:22 -0700

    Small changes to unzipping shell file

--- analysis_code/unsip_viz.sh
+++ analysis_code/unsip_viz.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#SBATCH -p RM-shared
+#SBATCH -t 5:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-type=ALL
 
-
+cd /pylon2/ca4s8kp/lwaldrop/entcode_finished
 for i in `seq 1 1233`; do 
-test -d viz_IB2d${i} && echo "File found" || echo "File ${i} not found."
+unzip viz_IB2d${i}.zip
+#test -d viz_IB2d${i} && echo "File found" || echo "File ${i} not found."
 #test -f viz_IB2d${i}.zip && echo "File found" || echo "File ${i} no found."
 done
