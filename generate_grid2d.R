d5bb0388c15ff7a324046ce66273017facfff069
lindsaywaldrop <waldrop@chapman.edu>
2020-06-26 10:43:02 -0700

    Fixing R grid generation script, now working

--- generate_grid2d.R
+++ generate_grid2d.R
@@ -3,22 +3,22 @@
 # dist = distance from antennule
 # theta = angle of center hair with positive x-axis
 #
-
-rm(list=ls())
-
 require(pracma)
 require(useful)
+
+
+rm(list=ls())
 
 circle<-function(center,radius,L,dx){
 	x_grid = seq(-radius,radius,by=dx)
 	y_grid = seq(-radius,radius,by=dx)
 	whole_grid = meshgrid(x_grid,y_grid)
 	
-	THETA=seq(0,2*pi,length=250)
-	RHO=array(1,250)*radius
+	THETA=c(seq(0,2*pi,length=250),0)
+	RHO=array(1,length(THETA))*radius
 	Z = array(1,length(RHO))*0
-	nap<-matrix(c(THETA,RHO,Z),nrow=250,ncol=3)
-	points<-pol2cart(nap)
+	nap<-matrix(c(THETA,RHO),nrow=length(THETA),ncol=2)
+	points<-pol2cart(RHO,THETA)
 	points<-as.data.frame(points)
 	
 	In<-inpolygon(whole_grid$X,whole_grid$Y,points$x,points$y,boundary=FALSE)
@@ -65,39 +65,39 @@ kappa_target = 1.0e-2        # target point penalty spring constant (Newton)
 # Antennule
 ant<-circle(c(0,0),0.5*adia,L,dx);
 aN<-size(ant$X,2)
-plot(Y~X,data=ant,xlim=c(0,0.17),ylim=c(-0.05,0.05))
+plot(Y~X,data=ant,xlim=c(-0.25,0.25),ylim=c(-0.25,0.25),pch=19)
 
 # Hair 1
 h1<-circle(c(hair1Centerx,hair1Centery),0.5*hdia,L,dx)
 h1N<-size(h1$X,2)
-points(Y~X,data=h1)
+points(Y~X,data=h1,pch=19)
 # Hair 2
 h2<-circle(c(hair2Centerx,hair2Centery),0.5*hdia,L,dx)
 h2N<-size(h2$X,2)
-points(Y~X,data=h2)
+points(Y~X,data=h2,pch=19)
 
 # Hair 3
 h3<-circle(c(hair3Centerx,hair3Centery),0.5*hdia,L,dx)
 h3N<-size(h3$X,2)
-points(Y~X,data=h3)
+points(Y~X,data=h3,pch=19)
 
 #############################################################################
 ## Write points to vertex file
 
 totalN<-aN+h1N+h2N+h3N
 
-#filename<-"hairs2.vertex"
-#if(file.exists(filename)) file.remove(filename)
-#cat(as.character(totalN),sep="\n",file=filename,append=TRUE)
-#for (i in 1:aN){
-#cat(c(as.character(ant$X[i])," ",as.character(ant$Y[i]),"\n"),file=filename,sep="",append=TRUE)	
-#}
-#for (i in 1:h1N){
-#cat(c(as.character(h1$X[i])," ",as.character(h1$Y[i]),"\n"),file=filename,sep="",append=TRUE)	
-#}
-#for (i in 1:h2N){
-#cat(c(as.character(h2$X[i])," ",as.character(h2$Y[i]),"\n"),file=filename,sep="",append=TRUE)	
-#}
-#for (i in 1:h3N){
-#cat(c(as.character(h3$X[i])," ",as.character(h3$Y[i]),"\n"),file=filename,sep="",append=TRUE)	
-#}
+filename<-"hairs2.vertex"
+if(file.exists(filename)) file.remove(filename)
+cat(as.character(totalN),sep="\n",file=filename,append=TRUE)
+for (i in 1:aN){
+cat(c(as.character(ant$X[i])," ",as.character(ant$Y[i]),"\n"),file=filename,sep="",append=TRUE)
+}
+for (i in 1:h1N){
+cat(c(as.character(h1$X[i])," ",as.character(h1$Y[i]),"\n"),file=filename,sep="",append=TRUE)
+}
+for (i in 1:h2N){
+cat(c(as.character(h2$X[i])," ",as.character(h2$Y[i]),"\n"),file=filename,sep="",append=TRUE)
+}
+for (i in 1:h3N){
+cat(c(as.character(h3$X[i])," ",as.character(h3$Y[i]),"\n"),file=filename,sep="",append=TRUE)
+}
