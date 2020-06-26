e9cc0b3e89a8c4ae518b26e9bbeda45a06c13c44
lindsaywaldrop <waldrop@chapman.edu>
2020-06-26 10:49:26 -0700

    Adding documentation to R file

--- generate_grid2d.R
+++ generate_grid2d.R
@@ -3,12 +3,14 @@
 # dist = distance from antennule
 # theta = angle of center hair with positive x-axis
 #
+
+#### Loads required packages ####
 require(pracma)
 require(useful)
 
+rm(list=ls()) # Clears workspace
 
-rm(list=ls())
-
+#### Defines functions ####
 circle<-function(center,radius,L,dx){
 	x_grid = seq(-radius,radius,by=dx)
 	y_grid = seq(-radius,radius,by=dx)
@@ -31,6 +33,8 @@ circle<-function(center,radius,L,dx){
 	return(circ)
 }
 
+##### Input parameter definitions ####
+
 th=20.286
 L = 2.0         # length of computational domain (m)
 N = 1024        # number of Cartesian grid meshwidths at the finest level of the AMR grid
@@ -38,6 +42,7 @@ dx = 1.0*(L/N)  # Cartesian mesh width (m)
 # Notes ~ Rule of thumb: 2 boundary points per fluid grid point. 
 #        vertex pts too far apart: flow thru boundary, too close: numerical weirdness
 NFINEST = 4  # NFINEST = 4 corresponds to a uniform grid spacing of h=1/64
+kappa_target = 1.0e-2        # target point penalty spring constant (Newton)
 
 hdia = 0.01     # Diameter of hair
 adia = 0.1     # Diameter of flagellum
@@ -48,6 +53,7 @@ dist = 0.02     # Distance between antennule and hair
 mindGap = (0.5*adia)+(0.5*hdia)+dist  # Calculate distance between hair centers
 width = GtD*hdia+hdia
 
+#### Calculates center positions (x,y) of each hair ####
 hair1Centerx = mindGap*cos(th2)
 hair1Centery = mindGap*sin(th2)
 
@@ -57,20 +63,18 @@ hair2Centery = hair1Centery+width*cos(th2)
 hair3Centerx = hair1Centerx+width*sin(th2)
 hair3Centery = hair1Centery-width*cos(th2)
 
-kappa_target = 1.0e-2        # target point penalty spring constant (Newton)
-
-#############################################################################
-## All of the hairs
+#### Produces points within defined hairs ####
 
 # Antennule
-ant<-circle(c(0,0),0.5*adia,L,dx);
-aN<-size(ant$X,2)
-plot(Y~X,data=ant,xlim=c(-0.25,0.25),ylim=c(-0.25,0.25),pch=19)
+ant<-circle(c(0,0),0.5*adia,L,dx);  # Produces points that define antennule
+aN<-size(ant$X,2)                   # Records number of points inside antennule
+plot(Y~X,data=ant,xlim=c(-0.25,0.25),ylim=c(-0.25,0.25),pch=19) #Plots antennule
 
 # Hair 1
 h1<-circle(c(hair1Centerx,hair1Centery),0.5*hdia,L,dx)
 h1N<-size(h1$X,2)
 points(Y~X,data=h1,pch=19)
+
 # Hair 2
 h2<-circle(c(hair2Centerx,hair2Centery),0.5*hdia,L,dx)
 h2N<-size(h2$X,2)
@@ -81,13 +85,13 @@ h3<-circle(c(hair3Centerx,hair3Centery),0.5*hdia,L,dx)
 h3N<-size(h3$X,2)
 points(Y~X,data=h3,pch=19)
 
-#############################################################################
-## Write points to vertex file
 
-totalN<-aN+h1N+h2N+h3N
+#### Write points to vertex file ####
 
-filename<-"hairs2.vertex"
-if(file.exists(filename)) file.remove(filename)
+totalN<-aN+h1N+h2N+h3N  # Calculates total number of points (first line of vertex file)
+
+filename<-"hairs2.vertex"   # Defines file name
+if(file.exists(filename)) file.remove(filename)  # Deletes file with that name if it exists
 cat(as.character(totalN),sep="\n",file=filename,append=TRUE)
 for (i in 1:aN){
 cat(c(as.character(ant$X[i])," ",as.character(ant$Y[i]),"\n"),file=filename,sep="",append=TRUE)
@@ -101,3 +105,5 @@ cat(c(as.character(h2$X[i])," ",as.character(h2$Y[i]),"\n"),file=filename,sep=""
 for (i in 1:h3N){
 cat(c(as.character(h3$X[i])," ",as.character(h3$Y[i]),"\n"),file=filename,sep="",append=TRUE)
 }
+
+
