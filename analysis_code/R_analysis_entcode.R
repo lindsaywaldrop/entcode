d466638169ade6d6fcbb819d4db3595d00b5c691
lindsaywaldrop <waldrop@chapman.edu>
2020-08-07 11:19:38 -0700

    Separating shear and leakiness calculations into different scripts

--- analysis_code/R_analysis_entcode.R
+++ analysis_code/R_analysis_entcode.R
@@ -1,90 +1,57 @@
 #################################################################################################################
 #################################################################################################################
 ###
-### Leakiness and Shear Calculations
+### Leakiness Calculations
 ###
 #################################################################################################################
 
-# Clears any previous data
-rm(list=ls())
+rm(list=ls()) # Clears any previous data
 
-# Calculate leakiness base (no hairs condition)
-domain = 2.0		   	 # length of computational domain, m
-dx = 0.0003255208       	     # Distance of mesh grid, m
-hair_dia = 0.01   	 	 # diameter of each hair, m
+#### Assign condition values ####
+norows<-3              # Total number of rows in the array. Options: "3", "4"
+nohairs<-12            # Total number of hairs in the array. Options: "12", "18"
+dirname<-"/Users/waldrop/Dropbox (Chapman)/EntIBAMR/entcode/"	
+rundir<-"runs_3row/"   # Runs directory. Options: "runs_3row/", "runs/"
+
+n = 12				 # number of simulations to analyze
+
 speed = 0.06      	 	 # fluid speed, m/s
 duration = 0.03   	 	 # duration of simulation, s
 sample = 5000			 # sampling rate
-arrayrow<-4   # number of array row
+leakiness<-matrix(data=0,nrow=n,ncol=norows)	# Allocates space for leakiness calculation
 
-#shear_pt = floor(sample*(0.3*hair_dia+0.5*hair_dia)/domain) # Calculates distance of shear measurement from hair center
+setwd(dirname) # Sets directory to main
 
-n = 20				 # number of simulations to analyze
-	
-dirname<-"/Users/waldrop/Dropbox (Chapman)/EntIBAMR/entcode/"	# Sets directory to main
-setwd(dirname)
-gaps<-read.table("analysis_code/gapwidths.txt",sep="")			# Loads file with gapwidth values in main directory
-
-leakiness = rep(0,n)					# Allocates space for leakiness calculation
-shear_hair<-data.frame(seq(1,n),seq(1,n),seq(1,n),seq(1,n),seq(1,n),seq(1,n))	# Allocates space for shear calculations
-names(shear_hair)<-c("hair1top","hair1bottom","hair2top","hair2bottom","hair3top","hair3bottom")
-
-#par(mfrow=c(1,3))
-plot(c(0),ylim=c(-0.1,0.1),xlim=c(0,sample))	# Optional plotting for running data
-
+#### Main Loop for Calculations ####
 for (j in 1:n){		# Main loop
 	print(paste("Simulation: ",j,sep=""))					# Prints simulation number 
-	# Construct directory name
-	dirname2<-paste(dirname,"runs/viz_IB2d",j,"/hairline",arrayrow,sep="")
-	# Sets working directory
-	setwd(dirname2)
-	# Loads final time-step data
-	data1 <- read.table("hairline0003.curve", header=FALSE, sep="")	
-	points(data1$V2,pch=19)
-	# Calculates bottom of leakiness ratio based on hair spacing for each simulation
-	#gapshere<-floor(sample*(gaps$V1[j]/domain))			# Calculates the gap distance in sampling units
-	#range_low<-half_sample-gapshere						# Establishes low point in sampling range 
-	#range_high<-half_sample+gapshere					# Establishes high point in sampling range
-	samplewidth<-gaps$V1[j]/sample
-	leak_bot = (speed*duration)*(gaps$V1[j])	# Calculates bottom of leakiness ratio
-	
-	data2=rep(0,length(data1$V1))		# Allocates space for leakiness calculation
-	
-	for (i in 1:sample+1){	# Loop that cycles through each sampled point in a simulation
-	  data2[i]=(data1$V2[i]*duration*samplewidth)	# Calculates top of leakiness for each sampled point
-	}
-	leakiness[j]=sum(data2)/leak_bot	# Calculates leakiness value for each simulation
-	rm(data2)
-	# Calculates shear shear_pt away from center of hair
-	# hair1t<-half_sample+shear_pt
-	# shear_hair$hair1top[j]<-data1$V2[hair1t]/speed
-	# hair1b<-half_sample-shear_pt-1
-	# shear_hair$hair1bottom[j]<-data1$V2[hair1b]/speed
-	# 
-	# hair2t<-range_high+shear_pt
-	# shear_hair$hair2top[j]<-data1$V2[hair2t]/speed
-	# hair2b<-range_high-shear_pt-1
-	# shear_hair$hair2bottom[j]<-data1$V2[hair2b]/speed	
-	# 
-	# hair3t<-range_low+shear_pt
-	# shear_hair$hair3top[j]<-data1$V2[hair3t]/speed
-	# hair3b<-range_low-shear_pt-1
-	# shear_hair$hair3bottom[j]<-data1$V2[hair3b]/speed
-		
-
+  for (arrayrow in 1:norows){
+    dirname2<-paste(dirname,rundir,"viz_IB2d",j,"/hairline",arrayrow,"/",sep="") # Construct directory name
+    data1 <- read.table(paste(dirname2,"hairline0003.curve",sep=""), header=FALSE, sep="")	# Loads final time-step data
+	  rowwidth<-data1$V1[sample+1]-data1$V1[1]
+	  samplewidth<-rowwidth/sample # Calculates real width between sample points
+	  leak_bot = (speed*duration)*rowwidth	# Calculates bottom of leakiness ratio
+	  data2=rep(0,length(data1$V1))		# Allocates space for leakiness calculation
+	  
+	  for (i in 1:sample+1){	# Loop that cycles through each sampled point in a simulation
+	    data2[i]=(data1$V2[i]*duration*samplewidth)	# Calculates top of leakiness for each sampled point
+	  }
+	  leakiness[j,arrayrow]<-sum(data2)/leak_bot	# Calculates leakiness value for each simulation
+	  rm(data2)
+  }
 }
 
-setwd(paste(dirname,"runs/",sep=""))	# Sets directory back to main
-plot(leakiness,ylim=c(0,max(leakiness)))		# Plots final values of leakiness for each simulation
 
-# plot(shear_hair$hair1top,ylim=c(0,2),pch=21, col="purple",bg="purple")
-# points(shear_hair$hair1bottom,pch=21, col="purple")
-# points(shear_hair$hair2top,pch=21,col="red",bg="red")
-# points(shear_hair$hair2bottom,pch=21,col="red")
-# points(shear_hair$hair3top,pch=21,col="blue",bg="blue")
-# points(shear_hair$hair3bottom,pch=21,col="blue")
+#plot(leakiness,ylim=c(0,1))		# Plots final values of leakiness for each simulation
 
-# Saves leakiness values
-write.table(leakiness,file=paste("leakiness-row",arrayrow,"-",n,"-",Sys.Date(),".csv",sep=""),sep=",")
-# write.table(shear_hair,file=paste("shear_hair",n,"-",Sys.Date(),".csv",sep=""),sep=",")
+leakiness2<-data.frame(leakiness)
+leaknames<-as.character(rep(0,norows)) # Allocates space for names 
+for (i in 1:norows){leaknames[i]<-paste("row",i,sep="")} # Assigns name for each hair
+names(leakiness2)<-leaknames # Assigns all names to data frame
+
+setwd(paste(dirname,rundir,sep=""))	# Sets directory back to main
+# Saves data!
+write.table(leakiness2,file=paste("leakiness-",n,"-",Sys.Date(),".csv",sep=""),sep=",")
+# Quits R
+#quit(save="no")
 #################################################################################################################
---
+++ analysis_code/R_analysis_shear.R
@@ -0,0 +1,93 @@
+#################################################################################################################
+#################################################################################################################
+###
+### Shear Calculations
+###
+#################################################################################################################
+
+rm(list=ls()) # Clears any previous data
+
+#### Assign condition values ####
+norows<-3              # Total number of rows in the array. Options: "3", "4"
+nohairs<-12            # Total number of hairs in the array. Options: "12", "18"
+dirname<-"/Users/waldrop/Dropbox (Chapman)/EntIBAMR/entcode/"	
+rundir<-"runs_3row/"
+
+# The following vector dictates which side the shear should be sampled on. 
+# The hair where sampling started (e.g. hair 2 for row 1) should be "-1".
+# The hair where sampling ended (e.g. hair 3 for row 1) should be "1".
+# All interior hairs should be "0".
+whichside<-c(0,-1,1,         # row 1
+             0,0,-1,1,       # row 2
+             -1,0,0,0,1,     # row 3
+             -1,0,0,0,0,1,   # row 4
+             -1,0,0,0,0,0,1) # row 5
+
+n = 12				         # number of simulations to analyze
+
+hair_dia = 0.002   	 	 # diameter of each hair, m
+sample = 5000			     # sampling rate
+shear_hair<-matrix(data=0,nrow=n,ncol=nohairs)	# Allocates space for shear calculations
+
+setwd(dirname) # Sets directory to main
+
+#### Main Loop for Calculations ####
+for (j in 1:n){		# Main loop
+  print(paste("Simulation: ",j,sep=""))					# Prints simulation number 
+  for (arrayrow in 1:norows){
+    
+    dirname2<-paste(dirname,rundir,"viz_IB2d",j,"/hairline",arrayrow,"/",sep="") # Construct directory name
+    data1 <- read.table(paste(dirname2,"hairline0003.curve",sep=""), header=FALSE, sep="")	# Loads final time-step data
+    rowwidth<-data1$V1[sample+1]-data1$V1[1] # Calculates real width of sampled region
+    samplewidth<-rowwidth/sample # Calculates real width between sample points
+    half_hair_width<-floor((0.5)*hair_dia/samplewidth) # Calculates number of sample points covered by hair radius
+    shear_point<-floor((1)*hair_dia/samplewidth) # Calculates number of sample points covered by hair diameter
+    
+    if (arrayrow==1){ # Assigning which hairs to run calculation
+      hairsinrow=3    # Sets the number of hairs in the current rwo
+      hairsdone=0     # Sets the hairs already calculated based on current row
+    }else if (arrayrow==2){
+      hairsinrow=4
+      hairsdone=3
+    }else if (arrayrow==3){
+      hairsinrow=5
+      hairsdone=7
+    }else if (arrayrow==4){
+      hairsinrow=6
+      hairsdone=12
+    }else{ 
+      hairsinrow=7
+      hairsdone=18
+      }
+    for (k in 1:hairsinrow){  
+      side<-whichside[k+hairsdone] # Assigns which side the shear calculation should be on
+      
+      if (side==-1){  # Sets start and end points of calculation based on which side 
+        stpt<-half_hair_width
+        endpt<-half_hair_width+shear_point
+      }else {
+        stpt<-(length(data1$V1)-(half_hair_width+shear_point))
+        endpt<-(length(data1$V1)-half_hair_width)
+      }
+      curve<-smooth.spline(data1$V1[stpt:endpt],data1$V2[stpt:endpt],spar=0.90) # Creates smooth spline fit of data
+      new.curve<-predict(curve,seq(min(data1$V1[stpt:endpt]),max(data1$V1[stpt:endpt]),by=0.00005)) # Resamples to create smooth spline curve
+      slopes<-diff(new.curve$y)/diff(new.curve$x)  # Calculates slopes between points of smooth spline
+      shear_hair[j,k+hairsdone]<-abs(mean(slopes,na.rm=TRUE)) # Calculates mean shear rate of all points in smooth spline
+    } # ends hairs in row loop
+  } # ends array row loop
+} # ends main loop
+
+#### Final Data Processing and Saving ####
+shear_hair2<-data.frame(shear_hair)  # Turns matrix into data frame
+shearnames<-as.character(rep(0,nohairs)) # Allocates space for names 
+for (i in 1:nohairs){shearnames[i]<-paste("hair",i,sep="")} # Assigns name for each hair
+names(shear_hair2)<-shearnames # Assigns all names to data frame
+
+setwd(paste(dirname,rundir,sep=""))	# Sets directory back to main
+# Save data!
+write.table(shear_hair2,file=paste("shearhairs-",n,"-",Sys.Date(),".csv",sep=""),sep=",")
+# Quits R
+#quit(save="no")
+#################################################################################################################
+
+
