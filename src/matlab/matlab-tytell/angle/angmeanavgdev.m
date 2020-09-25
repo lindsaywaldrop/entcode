e93d85a23d457cb70eb6c23247746ccf8356c141
lindsaywaldrop <waldrop@chapman.edu>
2020-09-25 10:01:50 -0700

    Removing matlab files that are unnecessary

--- src/matlab/matlab-tytell/angle/angmeanavgdev.m
+++
@@ -1,6 +0,0 @@
-function mad = angmeanavgdev(ang)
-
-ang = mod(ang,2*pi);
-med = angmedian(ang);
-
-mad = pi - nanmean(abs(pi - abs(ang - repmat(med,[size(ang,1) 1]))));
--- src/matlab/matlab-tytell/imcorrel/error.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "imcorrel.h"
-
-const char *ErrorText[NERR] = {
-	"Grid out of bounds.",
-	"Out of memory."
-};
