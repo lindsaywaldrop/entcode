bd9890774b18903a3654b6e2c13f76ea2321e87b
lindsaywaldrop <waldrop@chapman.edu>
2023-02-02 11:18:16 -0800

    Small changes to bridges scripts

--- src/r-scripts/generate_grid2d_multirow.R
+++ src/r-scripts/generate_grid2d_multirow.R
@@ -18,7 +18,7 @@ plotit <- 0
 # plot the hairs? yes = 1, no = 0
 startrun <- 1
 endrun <- 2000
-nohairs <- 3  # 2 row: 7; 3 row: 12; 4 row: 18; 5 row: 25
+nohairs <- 25  # 2 row: 7; 3 row: 12; 4 row: 18; 5 row: 25
 
 mainDir1 <- "./data/vertex-files"
 mainDir2 <- "./data/csv-files"
